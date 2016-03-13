555818a2a0dce369b42f1ad1ac6053d17b8a09bf
audreyseo <audreyleeseo@gmail.com>
2016-03-12 22:13:26 -0500

    Added the class file for the RoundedRectButton class. Haven't filled anything in though besides the constructor.

---
+++ FunGUI/src/fungui/RoundedRectButton.java
@@ -0,0 +1,11 @@
+package fungui;
+
+import processing.core.PApplet;
+
+public class RoundedRectButton extends RectButton {
+	
+	
+	public RoundedRectButton(PApplet p, float nx, float ny, float nw, float nh, int [] ncolors, String ntext) {
+		super(p, nx, ny, nw, nh, ncolors, ntext);
+	}
+}
