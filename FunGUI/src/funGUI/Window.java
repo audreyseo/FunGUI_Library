8ab9556a84f5d2d2b9fe5d2c8bd7d9f14f56842f
audreyseo <audreyleeseo@gmail.com>
2016-03-27 09:13:58 -0400

    I finally put together some of the window class, ie made a more detailed constructor.

--- FunGUI/src/funGUI/Window.java
+++ FunGUI/src/funGUI/Window.java
@@ -9,7 +9,34 @@ package funGUI;
  *
  */
 public class Window extends Display {
+	float horizontalMargin = 2;
+	float verticalMargin = 2;
+	
+	
+	/**
+	 * Constructor for the Window class.
+	 * @param x						float, x-coordinate of center of window
+	 * @param y						float, y-coordinate of center of window
+	 * @param w						float, width of the window
+	 * @param h						float, height of the window
+	 * @param horizontalMargin		float, basically the amount of area to keep clear in the x-direction along the edges
+	 * @param verticalMargin		float, the amount of area to keep clear in the y-direction along the edges
+	 */
+	public Window(float x, float y, float w, float h, float horizontalMargin, float verticalMargin) {
+		super(x, y, w, h);
+		this.horizontalMargin = horizontalMargin;
+		this.verticalMargin = verticalMargin;
+	}
+	
 	public Window(float x, float y, float w, float h) {
 		super(x, y, w, h);
+	}
+	
+	float innerWidth() {
+		return(w - 2 * horizontalMargin);
+	}
+	
+	float innerHeight() {
+		return(h - 2 * verticalMargin);
 	}
 }
