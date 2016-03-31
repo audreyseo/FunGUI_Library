872afd7345994241d3c5f5037adea1621fc8a7a0
audreyseo <audreyleeseo@gmail.com>
2016-03-30 21:52:11 -0400

    I might have fixed the calculator a little bit...The buttons didn't have a 9...

--- FunGUI/src/funGUI/Calculator.java
+++ FunGUI/src/funGUI/Calculator.java
@@ -2,7 +2,7 @@ package funGUI;
 import processing.core.*;
 
 public class Calculator extends Frame {
-	RoundButton [][] nums = new RoundButton [3][3];
+	RoundButton [][] nums = new RoundButton [4][4];
 	
 	public Calculator(PApplet p, float x, float y, float w, float h) {
 		this.p = p;
@@ -10,12 +10,18 @@ public class Calculator extends Frame {
 		this.y = y;
 		this.w = w;
 		this.h = h;
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = 0; j < nums[i].length; j++) {
-				String chars = Integer.toString(i * 3 + j);
-				nums[i][j] = new RoundButton(p, x + (i - 1) * 50, y + (j - 1) * 50, 20, chars.charAt(0));
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				String chars = Integer.toString(i + j * 3);
+				if (Integer.getInteger(chars) <= 8) {
+					nums[i][j] = new RoundButton(p, x + (i - 1) * 50, y + (j - 1) * 50, 20, chars.charAt(0));
+				} else {
+					nums[i][j] = new RoundButton(p, x + (i - 1) * 50, y + (j - 1) * 50, 20, ' ');
+				}
 			}
 		}
+		nums[0][3] = new RoundButton(p, x - 50, y + 100, 20, '9');
+		
 	}
 	
 	@Override
