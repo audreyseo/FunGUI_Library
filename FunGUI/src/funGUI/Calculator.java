4dfa1ea71ba42c2e3275bf7cce1f99b3d576e79b
audreyseo <audreyleeseo@gmail.com>
2016-03-30 22:07:03 -0400

    I added a few functions to assist in the actual typing of things into the calculator.

--- FunGUI/src/funGUI/Calculator.java
+++ FunGUI/src/funGUI/Calculator.java
@@ -3,9 +3,13 @@ import processing.core.*;
 
 public class Calculator extends Frame {
 	RoundButton [][] nums = new RoundButton [3][4];
+	boolean pressed = false;
+	String math = "";
+	PFont font;
 	
 	public Calculator(PApplet p, float x, float y, float w, float h) {
 		this.p = p;
+		this.g = p.g;
 		this.x = x;
 		this.y = y;
 		this.w = w;
@@ -23,16 +27,48 @@ public class Calculator extends Frame {
 		nums[0][3] = new RoundButton(p, x - 50, y + 100, 20, '0');
 		nums[1][3] = new RoundButton(p, x, y + 100, 20, '.');
 		nums[2][3] = new RoundButton(p, x + 50, y + 100, 20, 'c');
-		
+		font = p.createFont(REG_SANSS_TXT, LGTXTSIZE);
 	}
 	
 	@Override
-	public void draw() {
+	public void display() {
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = 0; j < nums[i].length; j++) {
 				nums[i][j].draw();
 			}
 		}
+		check();
+	}
+	
+	@Override
+	public void text() {
+		g.pushStyle();
+		g.textFont(font);
+		g.textAlign(CENTER, CENTER);
+		g.text(math, x, y - 70);
+		g.popStyle();
+	}
+	
+	void check() {
+		if (!pressed && p.mousePressed && anyPressed() >= 0) {
+			int n = anyPressed();
+			int n1 = (n >> 8) & 0xF;
+			int n2 = n & 0xF;
+			String s = String.valueOf(nums[n1][n2].c);
+			math += s;
+		}
+	}
+	
+	int anyPressed() {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				if (nums[i][j].clicked()) {
+					int index = (i << 8) + j;
+					return(index);
+				}
+			}
+		}
+		return(-1);
 	}
 	
 	
