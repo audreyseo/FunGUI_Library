c20c5d0e3a6dc6f2e3e6e5e23995f4d0a98c4423
audreyseo <audreyleeseo@gmail.com>
2016-03-30 21:46:52 -0400

    I added more functionality to the Calculator class, ie buttons (that currently don't do anything)

--- FunGUI/src/funGUI/Calculator.java
+++ FunGUI/src/funGUI/Calculator.java
@@ -2,6 +2,7 @@ package funGUI;
 import processing.core.*;
 
 public class Calculator extends Frame {
+	RoundButton [][] nums = new RoundButton [3][3];
 	
 	public Calculator(PApplet p, float x, float y, float w, float h) {
 		this.p = p;
@@ -9,8 +10,22 @@ public class Calculator extends Frame {
 		this.y = y;
 		this.w = w;
 		this.h = h;
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				String chars = Integer.toString(i * 3 + j);
+				nums[i][j] = new RoundButton(p, x + (i - 1) * 50, y + (i - 1) * 50, 20, chars.charAt(0));
+			}
+		}
 	}
 	
+	@Override
+	public void draw() {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				nums[i][j].draw();
+			}
+		}
+	}
 	
 	
 	@Override
