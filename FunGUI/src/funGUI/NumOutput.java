1024e153f824bf108303a84de6f5c8215a055d30
audreyseo <audreyleeseo@gmail.com>
2016-04-07 17:32:20 -0400

    I got tired of this slider-output stuff so I put a slider back into numoutput...

--- FunGUI/src/funGUI/NumOutput.java
+++ FunGUI/src/funGUI/NumOutput.java
@@ -4,7 +4,21 @@ import processing.core.*;
 public class NumOutput extends Frame implements PConstants {
 	float val = 0.0f;
 	PFont font;
+	String label;
+	Slider s;
 	
+	
+	public NumOutput(PApplet p, float x, float y, float w, float h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.p = p;
+		this.g = p.g;
+		val = 0;
+//		PApplet.println("finished init");
+		font = this.p.createFont("Times-New-Roman", (float) (h * .8), true);
+	}
 	
 	public NumOutput(PApplet p, float x, float y, float w, float h, float initialValue) {
 		this.x = x;
@@ -41,7 +55,7 @@ public class NumOutput extends Frame implements PConstants {
 	
 	@Override
 	protected void display() {
-		PApplet.println("displayed");
+//		PApplet.println("displayed");
 		g.pushStyle();
 		g.fill(255);
 		g.stroke(0);
@@ -52,7 +66,15 @@ public class NumOutput extends Frame implements PConstants {
 		g.textAlign(LEFT, CENTER);
 		g.text(val, x - (w * .45f), y);
 		g.popStyle();
-		PApplet.println("done displaying");
+//		PApplet.println("done displaying");
+	}
+	
+	float val() {
+		if (val == 0 && s != null) {
+			return(s.p());
+		} else {
+			return(val);
+		}
 	}
 	
 	public void assign(float f) {
